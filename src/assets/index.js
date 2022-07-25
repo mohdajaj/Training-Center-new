aac5e6487f392236b0d1f078c8d537d35a37f618
Mudit Srivastava <tnode7@chipsoftindia.com>
2022-07-25 12:46:02 +0530

    js changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,19 @@
+function resize() {
+  setTimeout(resizeWithDelay, 500);
+}
+
+function resizeWithDelay() {
+  var clientHeight = document.getElementById("mainDiv").scrollHeight;
+  console.log("resizeWithDelay function called.........");
+  parent.postMessage(
+    {
+      user_age: clientHeight,
+      user_height: $("input[name='user_height']").val(),
+    },
+    "*"
+  );
+}
+
 
 function resize()
 {
